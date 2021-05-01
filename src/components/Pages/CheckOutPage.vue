601172387b231d63f878ea955b5a0fbe7f95dcc3
William <tony05078088@gmail.com>
2021-05-01 13:09:04 +0800

    需要格式

--- src/components/Pages/CheckOutPage.vue
+++ src/components/Pages/CheckOutPage.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="checkoutPage">
-    <div>Checkout Page</div>
-    <p v-for="item in ingredients" :key="item.id">
-      {{ item.name }} For {{ item.quantity }} Each ${{ item.price }} ＝
-      {{ item.price * item.quantity }}
-    </p>
-    <p>Toal Amount to Paid: ${{ totalAmount }}</p>
+    <h2>Checkout Page</h2>
+    <div v-if="ingredients.length > 0" ref="checkout">
+      <p v-for="item in ingredients" :key="item.id" class="items">
+        {{ item.name }} For {{ item.quantity }} Each ${{ item.price }} ＝ ${{
+          item.price * item.quantity
+        }}
+      </p>
+      <p>Toal Amount to Paid: ${{ totalAmount }}</p>
+      <el-button type="primary" @click="GoCheckout">CheckOut</el-button>
+    </div>
+    <div v-else ref="NoGoods" class="noItems">
+      <p>No Items Yet</p>
+      <el-button type="primary" @click="GoCheckout">Back to Store</el-button>
+    </div>
   </div>
 </template>
 
@@ -13,10 +21,6 @@
 export default {
   props: ["ingredients"],
   computed: {
-    EachAmount() {
-      console.log(this.ingredients[0].name);
-      return this.ingredients.forEach(el => el.name);
-    },
     totalAmount() {
       let totalAmount = 0;
       this.ingredients.forEach(element => {
@@ -25,13 +29,46 @@ export default {
       });
       return totalAmount;
     }
+  },
+  mounted() {
+    console.log(this.$refs.checkout)
+  },
+  methods: {
+    GoCheckout() {
+      console.log(this.$refs)
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .checkoutPage {
+  position: relative;
   display: flex;
   flex-direction: column;
+  border: 1px solid black;
+  min-height: 200px;
+  height: 50%;
+  .items {
+    display: flex;
+    flex-direction: column;
+    min-height: 40%;
+    margin-top: 5%;
+  }
+  button {
+    width: 100%;
+    position: absolute;
+    bottom: 0%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  p {
+    min-height: 40%;
+    height: 40%;
+  }
+  .noItems {
+    min-height: 40%;
+    height: 50%;
+  }
 }
 </style>
