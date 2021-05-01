158895df88202f52172eb418c694f8a2681e1848
William <tony05078088@gmail.com>
2021-05-01 14:07:29 +0800

    try

--- src/components/Pages/CheckOutPage.vue
+++ src/components/Pages/CheckOutPage.vue
@@ -2,11 +2,7 @@
   <div class="checkoutPage">
     <div>Checkout Page</div>
     <div v-if="ingredients.length > 0" class="checkout">
-      <el-table
-        :data="ingredients"
-        border
-        style="width: 100%"
-      >
+      <el-table :data="ingredients" border style="width: 100%">
         <el-table-column prop="name" label="Item Name" width="180">
         </el-table-column>
         <el-table-column prop="price" label="Price" width="180">
@@ -14,6 +10,7 @@
         <el-table-column prop="quantity" label="Quantity"> </el-table-column>
       </el-table>
       <p>Toal Amount to Paid: ${{ totalAmount }}</p>
+      <el-button type="primary" @click="GoCheckout">CheckOut</el-button>
     </div>
     <div v-else class="noItems">
       <p>NOItems</p>
@@ -25,10 +22,6 @@
 export default {
   props: ["ingredients"],
   computed: {
-    EachAmount() {
-      console.log(this.ingredients[0].name);
-      return this.ingredients.forEach(el => el.name);
-    },
     totalAmount() {
       let totalAmount = 0;
       this.ingredients.forEach(element => {
@@ -36,6 +29,11 @@ export default {
         totalAmount += EachAmount;
       });
       return totalAmount;
+    }
+  },
+  methods: {
+    GoCheckout() {
+      console.log("ok");
     }
   }
 };
